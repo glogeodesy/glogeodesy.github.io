d4f24ed72a9fd742c1c8b949b570e3657b60b0fe
glogeodesy <glo@geodesy.cn>
2015-05-05 01:50:01 +0000

    update

--- xgit.cpp
+++
@@ -1,9 +0,0 @@
-#include <cstdlib>
-using namespace std;
-int main()
-{
-    std::system("git add --all");
-    std::system("git commit -m \"update\"");
-    std::system("git push origin master");
-	return 0;
-}
